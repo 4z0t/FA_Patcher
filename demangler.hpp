c20b8960a7e8c76c71547397d42e8dc8a3561d75
Alexander <84857900+4z0t@users.noreply.github.com>
2023-09-09 18:07:18 +0300

    add files

---
+++ demangler.hpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <io.h>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <stdio.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include "utility.hpp"
+
+
+
+
---
+++ utility.hpp
@@ -0,0 +1,21 @@
+
+#include <algorithm>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <io.h>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <stdio.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#define ErrLog(str) \
+    cerr << __FUNCTION__ << ":" << __LINE__ << str << "\n"
+
+bool starts_with(std::string str, const char *substr)
+{
+    return strncmp(str.c_str(), substr, strlen(substr)) == 0;
+}
