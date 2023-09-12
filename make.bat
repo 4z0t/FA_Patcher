18ff99bc13af837a7d50d9efeff249a27ec4806c
Alexander <84857900+4z0t@users.noreply.github.com>
2023-09-12 14:38:55 +0300

    remove xcopy

--- make.bat
+++ make.bat
@@ -3,4 +3,3 @@ g++ -c -Os -std=c++17 utility.cpp -o utility.o
 g++ -c -Os -std=c++17 main.cpp -o main.o
 g++ -c -Os -std=c++17 FunctionMapper.cpp -o FunctionMapper.o
 g++ -pipe -static -s demangler.o utility.o FunctionMapper.o main.o -o FaP.exe
-xcopy /y FaP.exe C:\Users\4z0t\GIT\fa_patch\FA-Binary-Patches\FaP.exe
